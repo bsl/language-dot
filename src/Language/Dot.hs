fb248bae567c534b1cc121c8f19f3225e212a5c8
Brian Lewis <brian@lorf.org>
2009-06-29 11:53:27 -0500

    Fixed Dot.hs export list.

--- src/Language/Dot.hs
+++ src/Language/Dot.hs
@@ -1,4 +1,11 @@
 module Language.Dot
   (
+    parseDotData
+  , parseDotFile
+  , module Language.Dot.Parser
+  , module Language.Dot.Syntax
   )
   where
+
+import Language.Dot.Parser
+import Language.Dot.Syntax
